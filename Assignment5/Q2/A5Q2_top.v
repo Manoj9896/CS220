520d62e6100c58463a337625500481808960abd1
Manoj9896 <manojkumar.nvsd@gmail.com>
2021-03-11 15:22:54 +0530

    Assignment5

--- Assignment5/Q2/A5Q2_top.v
+++ Assignment5/Q2/A5Q2_top.v
@@ -10,15 +10,27 @@ module A5Q2_top;
     initial begin
         $monitor("nums[0]=%d nums[1]=%d nums[2]=%d nums[3]=%d index=%d",nums[0],nums[1],nums[2],nums[3],index);
         nums[0]=1;nums[1]=2;nums[2]=3;nums[3]=4;
-        #2
-        nums[0]=5;nums[1]=2;nums[2]=3;nums[3]=4;
-        #2
-        nums[0]=6;nums[1]=2;nums[2]=0;nums[3]=4;
-        #2
-        nums[0]=1;nums[1]=2;nums[2]=3;nums[3]=1;
-        #2
-        nums[0]=7;nums[1]=2;nums[2]=3;nums[3]=0;
-        #2
+        #1
+        nums[0]=5;nums[1]=2;nums[2]=6;nums[3]=4;
+        #1
+        nums[0]=7;nums[1]=6;nums[2]=3;nums[3]=7;
+        #1
+        nums[0]=5;nums[1]=2;nums[2]=3;nums[3]=0;
+        #1
+        nums[0]=2;nums[1]=2;nums[2]=2;nums[3]=0;
+        #1
+        nums[0]=6;nums[1]=2;nums[2]=3;nums[3]=7;
+        #1
+        nums[0]=0;nums[1]=0;nums[2]=0;nums[3]=0;
+        #1
+        nums[0]=5;nums[1]=3;nums[2]=3;nums[3]=4;
+        #1
+        nums[0]=7;nums[1]=6;nums[2]=5;nums[3]=4;
+        #1
+        nums[0]=3;nums[1]=1;nums[2]=0;nums[3]=5;
+        #1
+        nums[0]=5;nums[1]=5;nums[2]=5;nums[3]=5;
+        #1
         $finish;
     end
 
